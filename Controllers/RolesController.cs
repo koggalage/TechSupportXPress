354e42d2f95e4060a0fa14aaacef5ca80f4f2fc5
thilinacz <koggalage@gmail.com>
2025-03-02 01:10:25 +0530

    #Added User Roles Creation

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechSupportXPress.Data;
+using TechSupportXPress.Models;
+using TechSupportXPress.ViewModels;
+
+namespace TechSupportXPress.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _rolemanager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public RolesController(ApplicationDbContext context,
+                               UserManager<ApplicationUser> userManager,
+                               RoleManager<IdentityRole> rolemanager,
+                               SignInManager<ApplicationUser> signInManager)
+        {
+            _rolemanager = rolemanager;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var roles = await _context.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(RolesViewModel vm)
+        {
+            IdentityRole role = new();
+            role.Name = vm.RoleName;
+
+            var result = await _rolemanager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(vm);
+            }
+        }
+
+    }
+}
---
+++ ViewModels/RolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace TechSupportXPress.ViewModels
+{
+    public class RolesViewModel
+    {
+
+        [DisplayName("Role No")]
+        public string Id { get; set; }
+
+
+        [DisplayName("Role Name")]
+        public string RoleName { get; set; }
+    }
+}
